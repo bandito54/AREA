fe390b3fe57443acf3dc2fe0c64d48037fde5982
Manu <emmanuel.adrian@epitech.eu>
2016-12-13 17:38:49 +0100

    facebook_start

--- facebook/src/main/java/org/facebook/App.java
+++ facebook/src/main/java/org/facebook/App.java
@@ -1,13 +1,31 @@
 package org.facebook;
 
-/**
- * Hello world!
- *
- */
+import facebook4j.Facebook;
+//import com.mashape.unirest.http.Unirest;
+//import com.mashape.unirest.http.exceptions.UnirestException;
+import facebook4j.*;
+import java.util.List;
+import facebook4j.conf.ConfigurationBuilder;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ConfigurationBuilder cb = new ConfigurationBuilder();
+ 
+    	cb.setDebugEnabled(true)
+    		.setOAuthAppId("390280877977708")
+    		.setOAuthAppSecret("******************************************")
+    		.setOAuthAccessToken("**************************************************")
+    		.setOAuthPermissions("email,publish_stream,...");//public_profile / user_friends / mail | utilisation sans certificat
+    	
+    	FacebookFactory ff = new FacebookFactory(cb.build());
+    	Facebook facebook = ff.getInstance();
+    	facebook.setOAuthAppId("390280877977708", "toto");
+    	facebook.setOAuthPermissions(commaSeparetedPermissions);
+    	facebook.setOAuthAccessToken(new AccessToken(accessToken, null));
+    	
+    	facebook.getOAuthAppAccessToken();
+    	facebook.postStatusMessage("Hello World from Facebook4J.");
     }
 }
