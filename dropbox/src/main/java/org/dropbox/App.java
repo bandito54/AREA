0de1915c4ef1d4f847ca0661243c5cca83a410c7
Thomas BARIC <Thomas BARIC@10.18.206.246>
2016-12-20 18:03:48 +0100

    Google Drive tu choisis tu compte zinc

--- dropbox/src/main/java/org/dropbox/App.java
+++ dropbox/src/main/java/org/dropbox/App.java
@@ -1,45 +1,44 @@
 package org.dropbox;
 
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-
 import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.*;
 import com.google.api.services.drive.Drive;
-
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.List;
 
-public class App {
+public class App
+{
     /** Application name. */
     private static final String APPLICATION_NAME =
         "Drive API Java Quickstart";
-
     /** Directory to store user credentials for this application. */
     private static final java.io.File DATA_STORE_DIR = new java.io.File(
         System.getProperty("user.home"), ".credentials/drive-java-quickstart");
-
     /** Global instance of the {@link FileDataStoreFactory}. */
     private static FileDataStoreFactory DATA_STORE_FACTORY;
-
     /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY =
         JacksonFactory.getDefaultInstance();
-
     /** Global instance of the HTTP transport. */
     private static HttpTransport HTTP_TRANSPORT;
-
     /** Global instance of the scopes required by this quickstart.
      *
      * If modifying these scopes, delete your previously saved credentials
@@ -47,7 +46,6 @@ public class App {
      */
     private static final List<String> SCOPES =
         Arrays.asList(DriveScopes.DRIVE_METADATA_READONLY);
-
     static {
         try {
             HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -58,15 +56,12 @@ public class App {
         }
     }
 
-    /**
-     * Creates an authorized Credential object.
-     * @return an authorized Credential object.
-     * @throws IOException
-     */
     public static Credential authorize() throws IOException, Exception {
         // Load client secrets.
-        InputStream in;
-        in = App.class.getResourceAsStream("/client_secret.json");
+        //InputStream in;
+        //in = App.class.getResourceAsStream("/client_secret.json");
+        InputStream in = new FileInputStream("C:\\Users\\Thomas BARIC\\workspace\\client_secret.json");
+        //C:\\Users\\myfiles\\Documents\\test5.xls
         GoogleClientSecrets clientSecrets =
             GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
@@ -74,7 +69,7 @@ public class App {
         GoogleAuthorizationCodeFlow flow =
                 new GoogleAuthorizationCodeFlow.Builder(
                         HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(DATA_STORE_FACTORY)
+                //.setDataStoreFactory(DATA_STORE_FACTORY)
                 .setAccessType("offline")
                 .build();
         Credential credential;
@@ -89,28 +84,34 @@ public class App {
      * @return an authorized Drive client service
      * @throws IOException
      */
-    public static Drive getDriveService() throws IOException, Exception {
+    
+    
+    public static Drive GetDriveService() throws IOException, Exception {
         Credential credential = authorize();
         return new Drive.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
-
+    
     public static void main(String[] args) throws IOException, Exception {
         // Build a new authorized API client service.
-        Drive service = getDriveService();
+        Drive service = GetDriveService();
 
         // Print the names and IDs for up to 10 files.
         FileList result = service.files().list()
              .setMaxResults(10)
              .execute();
         List<File> files = result.getItems();
-        if (files == null || files.isEmpty()) {
+        if (files == null || files.isEmpty())
+        {
             System.out.println("No files found.");
-        } else {
+        }
+        else
+        {
             System.out.println("Files:");
-            for (File file : files) {
+            for (File file : files)
+            {
                 System.out.printf("%s (%s)\n", file.getTitle(), file.getId());
             }
         }
